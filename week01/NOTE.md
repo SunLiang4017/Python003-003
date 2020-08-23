6f4c709981ae3bfa4687db9c770cdde6928f879c
Sun Liang <ping2405@gmail.com>
2020-08-24 00:09:54 +0900

    作业一

--- week01/NOTE.md
+++ week01/NOTE.md
@@ -1,1 +1,23 @@
 学习笔记
+
+这周的学习实践下来，最有帮助的是环境配置的时候使用venv。
+因为我之前的mac里面充斥了各种版本的python，用了pip安装了包还是找不到，非常麻烦。
+每开始一个工程都建立一个自己的venv的话，非常干净，不费力。
+python3 -m venv venv1
+source venv1/bin/activate
+pip install -r requirements.txt 
+
+查看版本：
+python -V
+pip -V
+which python
+
+安装初始化scrapy
+pip install scrapy
+scrapy startproject spiders
+cd spriders
+scrapy genspider example example.com
+执行scrapy
+scrapy crawl douban
+
+抓取猫眼的时候，最开始几次还能行，后来就只能验证中心的画面了。
---
+++ week01/maoyan_requests.py
@@ -0,0 +1,69 @@
+# 翻页的处理
+
+import requests
+import lxml.etree
+from bs4 import BeautifulSoup as bs
+from time import sleep
+import pandas as pd
+
+def getData(url):
+    print(url)
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
+
+    # 声明为字典使用字典的语法赋值
+    header = {}
+    header['user-agent'] = user_agent
+    response = requests.get('https://maoyan.com' + url, headers=header)
+
+    # xml化处理
+    selector = lxml.etree.HTML(response.text)
+
+    # 电影名称
+    film_name = selector.xpath('//h1[@class="name"]/text()')
+    print(f'电影名称: {film_name}')
+
+    # 电影类型
+    film_category = selector.xpath('/html/body/div[3]/div/div[2]/div[1]/ul/li[1]/a[1]/text()')
+    print(f'电影类型: {film_category}')
+
+    # 上映时间
+    plan_date = selector.xpath('/html/body/div[3]/div/div[2]/div[1]/ul/li[3]/text()')
+    print(f'上映时间: {plan_date}')
+
+    mylist = [film_name, plan_date, rating]
+
+    movie1 = pd.DataFrame(data = mylist)
+
+    # windows需要使用gbk字符集
+    movie1.to_csv('./movie1.csv', encoding='utf8', index=False, header=False)
+
+
+myurl = 'https://maoyan.com/films?showType=3'
+
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
+
+header = {'user-agent':user_agent}
+response = requests.get(myurl,headers=header)
+print(response.status_code)
+bs_info = bs(response.text, 'html.parser')
+print(bs_info)
+count = 0
+
+# Python 中使用 for in 形式的循环,Python使用缩进来做语句块分隔
+for atags in bs_info.find_all('a', attrs={'data-act': 'movie-click'}):
+    print(atags)
+    # 获取链接
+    url = atags.get('href')
+    if url == "/films" : continue
+    print(url)
+
+    if count > 10: break
+
+    getData(url)
+
+    count +=1
+    sheep(10)
+
+
+
+
